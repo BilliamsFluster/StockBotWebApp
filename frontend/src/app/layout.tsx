3085d18faf1fe629a6e927403b2e2eaac207ae3f
William <ww27hockey@icloud.com>
2025-07-22 13:01:29 -0400

    fixed hydration error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,29 @@
-// app/layout.tsx
+'use client';
+
 import './globals.css';
 import type { ReactNode } from 'react';
+import { useState, useEffect } from 'react';
 import Providers from '@/components/Providers';
 import { Toaster } from 'react-hot-toast';
 import AuthRedirect from '@/components/Auth/AuthRedirect';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <html lang="en">
       <body>
-        <Providers>
-          <AuthRedirect /> 
-          {children}
-          <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
-        </Providers>
+        {isClient && (
+          <Providers>
+            <AuthRedirect />
+            {children}
+            <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+          </Providers>
+        )}
       </body>
     </html>
   );
